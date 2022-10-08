b74a5acdd207815a590a92b57f3ccc9bde064ccd
Sneider <electronsgm@gmail.com>
2022-10-08 13:04:45 -0500

    Adding responsive functionality to the navigation bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,44 @@
 import './Navbar.css';
 import aerolines from '../../data/index.js';
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 
 function Navbar() {
     
-    useEffect(() => {
-    }, [])
+    const [responsive, setResponsive] = useState({
+        list: 'airline-list',
+        button: 'close'
+    })
+
+    function seeResponsiveMenu(){
+        if(responsive.button === 'close'){
+            setResponsive({
+                list: 'airline-list-responsive',
+                button: 'open'
+            })
+        }
+        if(responsive.button === 'open'){
+            setResponsive({
+                list: 'airline-list',
+                button: 'close'
+            })
+        }
+    }
 
     return (
-        <header >
+        <header className="header">
+            <h1 className="header-logo">
+                TravelFree
+            </h1>
             <nav className="airline-navbar">
-                <ol className="airline-list">
+                <ul className={responsive.list}>
                     {aerolines.map(aeroline => {
                     return <li className="airline-item" key={aeroline.id}>{aeroline.name}</li>
                     })}
-                </ol>
+                </ul>
             </nav>
+            <i id="menu-icon" class="material-icons" onClick={seeResponsiveMenu}>
+                {responsive.button === 'close' ? 'menu' : 'close'}
+            </i>
         </header>
     );
 }
